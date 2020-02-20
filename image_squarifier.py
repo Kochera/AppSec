e24f911d7f4eeada719499fe35bad64b59880444
Michael Kochera <mak922@nyu.edu>
2020-02-20 18:53:20 -0500

    Finish Squarifier Code

---
+++ image_squarifier.py
@@ -0,0 +1,24 @@
+from PIL import Image
+
+
+
+def make_square(im, min_size=256, fill_color=(255, 0, 0, 1)):
+    x, y = im.size
+    size = max(min_size, x, y)
+    new_im = Image.new('RGBA', (size, size), fill_color)
+    new_im = im.resize((size,size))
+    return new_im
+
+if __name__=="__main__":
+    done = False
+    while(done == False):
+        image = raw_input("Please enter a path to an image: ")
+        try:
+            test_image = Image.open(image)
+            new_image = make_square(test_image)
+            new_image.show()
+            new_image.save("squaredPic.png")
+            done = True
+        except:
+            raise OSError("Improper Path")
+        
