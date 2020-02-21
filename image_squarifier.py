e2be009cf54766c01cca944a4acfbe6efb686a05
Michael Kochera <mak922@nyu.edu>
2020-02-20 19:02:56 -0500

    Edited image_squarifier.py

--- image_squarifier.py
+++ image_squarifier.py
@@ -2,10 +2,9 @@ from PIL import Image
 
 
 
-def make_square(im, min_size=256, fill_color=(255, 0, 0, 1)):
+def make_square(im, min_size=256):
     x, y = im.size
     size = max(min_size, x, y)
-    new_im = Image.new('RGBA', (size, size), fill_color)
     new_im = im.resize((size,size))
     return new_im
 
@@ -17,7 +16,7 @@ if __name__=="__main__":
             test_image = Image.open(image)
             new_image = make_square(test_image)
             new_image.show()
-            new_image.save("squaredPic.png")
+            new_image.save("squaredImage.png")
             done = True
         except:
             raise OSError("Improper Path")
